0997a07f6e08dd84e996c8a3b28f1564835e3fa5
webtelsys <webtelsys@gmail.com>
2025-03-01 17:12:40 +0530

    first commit

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as process from "process";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  //await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT, hostname: '0.0.0.0');
 }
 bootstrap();
